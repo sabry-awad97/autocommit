6365029699b0c9dae7b0459eaa808c766f49a5c3
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-24 01:26:19 +0200

    🆕 feat(main.rs): add structopt and tokio dependencies and create Cli struct The `structopt` dependency has been added to enable the use of command-line argument parsing in the application. The `tokio` dependency has also been added to enable asynchronous programming in Rust. A new struct `Cli` has been created using the `StructOpt` derive macro, which defines the metadata for the command-line interface. The `main` function has also been updated to return a `Result` type with an empty `Ok(())` value, which is necessary for `tokio` to work correctly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "commit-master",
+    version = "1.0.0",
+    about = "A powerful CLI tool that helps you create professional and meaningful commits with ease, using AI to generate impressive commit messages in seconds. Take control of your code history and make it shine with commit-master!",
+    alias = "cm"
+)]
+struct Cli {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
