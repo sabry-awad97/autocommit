41ff9dd8c7a128c240f1805caf31ed922f5554ac
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-24 17:10:37 +0200

    🎨 style(main.rs): refactor CLI enum into struct and Command enum This commit refactors the CLI enum into a struct with a single field `command` of type Command enum. This improves readability and scalability of the codebase by allowing for adding more subcommands in the future without having to change the CLI enum.
    Also, the Command enum now contains all the subcommands previously in the CLI enum. This provides better separation of concerns between the different subcommands and their respective implementations.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,13 @@ use commands::{
     about = "A powerful CLI tool that helps you create professional and meaningful commits with ease, using AI to generate impressive commit messages in seconds. Take control of your code history and make it shine with autocommit!",
     alias = "ac"
 )]
-enum CLI {
+struct CLI {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
     #[structopt(name = "config")]
     ConfigCommand(ConfigCommand),
     #[structopt(name = "commit")]
@@ -25,11 +31,13 @@ enum CLI {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    match CLI::from_args() {
-        CLI::ConfigCommand(config) => {
+    let cli = CLI::from_args();
+
+    match cli.command {
+        Command::ConfigCommand(config) => {
             config.run()?;
         }
-        CLI::CommitCommand(commit) => {
+        Command::CommitCommand(commit) => {
             let config = get_config()?;
             commit.run(&config)?;
         }
