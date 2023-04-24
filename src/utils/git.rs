3d49ce7fe7bc45e95d363df406f010c13afba86f
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-24 19:42:49 +0200

    🔨 refactor(git.rs): refactor exclude_from_diff to use format macro This commit refactors `exclude_from_diff` method to use the `format!` macro instead of using string concatenation.
    🔨 refactor(git.rs): remove unused function get_detected_message
    This commit removes the unused function `get_detected_message` from `git.rs`.

--- src/utils/git.rs
+++ src/utils/git.rs
@@ -35,7 +35,7 @@ pub async fn get_staged_diff(files: &[String]) -> anyhow::Result<String> {
     let lock_files = files
         .iter()
         .filter(|file| file.contains(".lock") || file.contains("-lock."))
-        .map(|s| exclude_from_diff(s))
+        .map(|s| format!(":(exclude){}", s))
         .collect::<Vec<_>>();
 
     if !lock_files.is_empty() {
@@ -65,31 +65,11 @@ pub async fn get_staged_diff(files: &[String]) -> anyhow::Result<String> {
     Ok(diff)
 }
 
-fn exclude_from_diff(path: &str) -> String {
-    format!(":(exclude){}", path)
-}
-
-pub fn get_detected_message(files: &[String]) -> String {
-    format!(
-        "Detected {} staged file{}",
-        files.len(),
-        if files.len() > 1 { "s" } else { "" }
-    )
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     #[tokio::test]
     async fn test_assert_git_repo() {
         assert!(assert_git_repo().await.is_ok());
-    }
-
-    #[test]
-    fn test_get_detected_message() {
-        let files = vec!["file1.txt".to_string(), "file2.txt".to_string()];
-        assert_eq!(get_detected_message(&files), "Detected 2 staged files");
-        let files = vec!["file1.txt".to_string()];
-        assert_eq!(get_detected_message(&files), "Detected 1 staged file");
     }
 }
