d2c966e2215e7d31682904c821c9243c64d65259
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-24 18:07:11 +0200

    🎨 style(git.rs): refactor get_detected_message and exclude_from_diff functions This commit refactors the get_detected_message and exclude_from_diff functions for improved clarity and readability. The get_detected_message function now uses the ternary operator to determine whether to use "staged file" or "staged files" based on the number of files passed in. The exclude_from_diff function has been created to improve the readability of the git diff command by using the : (exclude) git pathspec magic signature to exclude the specified path from the git diff output.

--- src/utils/git.rs
+++ src/utils/git.rs
@@ -7,7 +7,12 @@ pub async fn assert_git_repo() -> anyhow::Result<()> {
         .arg("--is-inside-work-tree")
         .output()
         .await
-        .map_err(|e| anyhow!("Command 'git rev-parse --is-inside-work-tree' failed: {}", e))?;
+        .map_err(|e| {
+            anyhow!(
+                "Command 'git rev-parse --is-inside-work-tree' failed: {}",
+                e
+            )
+        })?;
 
     if !output.status.success() {
         panic!("The current working directory is not a Git repository");
@@ -16,11 +21,31 @@ pub async fn assert_git_repo() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn exclude_from_diff(path: &str) -> String {
+    format!(":(exclude){}", path)
+}
+
+pub fn get_detected_message(files: &[String]) -> String {
+    format!(
+        "Detected {} staged file{}",
+        files.len(),
+        if files.len() > 1 { "s" } else { "" }
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[tokio::test]
     async fn test_assert_git_repo() {
         assert!(assert_git_repo().await.is_ok());
+    }
+
+    #[test]
+    fn test_get_detected_message() {
+        let files = vec!["file1.txt".to_string(), "file2.txt".to_string()];
+        assert_eq!(get_detected_message(&files), "Detected 2 staged files");
+        let files = vec!["file1.txt".to_string()];
+        assert_eq!(get_detected_message(&files), "Detected 1 staged file");
     }
 }
