9b181f0512bfcb732e98e6ac95cac73fa1c37aa6
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-29 02:35:37 +0300

    🆕 feat(is_unicode_supported.rs): add a utility function to check unicode support A new utility function `is_unicode_supported()` is added that checks if the current environment supports Unicode. If the current environment supports Unicode, the function returns `true`. Otherwise, it returns `false`. The function is used to set a global static variable `UNICODE_SUPPORTED` which is used in the `get_unicode_string()` function to return the Unicode string if Unicode is supported, or a fallback string otherwise. The purpose of this change is to make the application more user-friendly by providing fallback support for environments that do not support Unicode.

---
+++ src/utils/is_unicode_supported.rs
@@ -0,0 +1,37 @@
+use std::env;
+
+use structopt::lazy_static::lazy_static;
+
+fn is_unicode_supported() -> bool {
+    if cfg!(windows) {
+        return env::var("CI").is_ok()
+            || env::var("WT_SESSION").is_ok()
+            || env::var("TERMINUS_SUBLIME").is_ok()
+            || env::var("ConEmuTask")
+                .map(|s| s == "{cmd::Cmder}")
+                .unwrap_or(false)
+            || env::var("TERM_PROGRAM")
+                .map(|s| s == "Terminus-Sublime" || s == "vscode")
+                .unwrap_or(false)
+            || env::var("TERM")
+                .map(|s| s == "xterm-256color" || s == "alacritty")
+                .unwrap_or(false)
+            || env::var("TERMINAL_EMULATOR")
+                .map(|s| s == "JetBrains-JediTerm")
+                .unwrap_or(false);
+    } else {
+        return env::var("TERM").map(|s| s != "linux").unwrap_or(true);
+    }
+}
+
+lazy_static! {
+    static ref UNICODE_SUPPORTED: bool = is_unicode_supported();
+}
+
+pub fn get_unicode_string<'a>(unicode_string: &'a str, fallback_string: &'a str) -> &'a str {
+    if *UNICODE_SUPPORTED {
+        unicode_string
+    } else {
+        fallback_string
+    }
+}
