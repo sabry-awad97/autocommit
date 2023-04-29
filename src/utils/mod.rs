023b8b659d46c203d274d0ec7049b8c64bf43337
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-29 05:53:44 +0300

    🎉 feat(utils): add `Spinner` module A new module named `Spinner` has been added to the `utils` module. The `Spinner` module provides a spinner UI component that can be used to indicate a loading state in the command-line interface. This component can be imported and used throughout the application.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,9 +3,11 @@ mod git;
 mod is_unicode_supported;
 mod openai;
 mod prompts;
+mod spinner;
 
 pub use chroma::Colors;
 pub use git::{assert_git_repo, get_changed_files, get_staged_diff, get_staged_files, git_add};
 pub use is_unicode_supported::get_unicode_string;
 pub use openai::{generate_message, Message, MessageRole};
 pub use prompts::{intro, outro};
+pub use spinner::Spinner;
