0e853e3396d7656193054dce85cf0a77a6354acd
Sabry Awad <dr.sabry1997@gmail.com>
2023-04-29 07:27:57 +0300

    🔨 refactor(spinner.rs): replace Spinner struct with spinner function The Spinner struct has been replaced with a function named `spinner` that takes a string message as input and returns a `spinergy::Spinner` struct. The function uses the `spinergy` crate to generate the spinner with the given message and style. The spinner function provides a cleaner and simpler way to create a spinner, and it also allows for greater flexibility in configuring the spinner.

--- src/utils/spinner.rs
+++ src/utils/spinner.rs
@@ -1,62 +1,12 @@
-use std::io::Write;
-use std::sync::mpsc::{channel, Sender};
-use std::thread;
-use std::time::Duration;
+use spinergy::{Color, Spinner, SpinnerConfig, SpinnerStyle};
 
 use crate::utils::{get_unicode_string, Colors};
 
-#[derive(Clone)]
-pub struct Spinner {
-    data: Vec<&'static str>,
-    sender: Option<Sender<()>>,
-    message: String,
-}
-
-impl Spinner {
-    pub fn new<T: Into<String>>(message: T) -> Self {
-        let mut message = message.into();
-        while message.ends_with('.') {
-            message.pop();
-        }
-        Self {
-            data: vec!["◒", "◐", "◓", "◑"],
-            sender: None,
-            message: message.into(),
-        }
-    }
-
-    pub fn start(&mut self) {
-        let (sender, receiver) = channel();
-        self.sender = Some(sender);
-        let mut spinner = self.clone();
-        thread::spawn(move || {
-            spinner.spin(receiver);
-        });
-    }
-
-    pub fn stop(&mut self) {
-        if let Some(sender) = &self.sender {
-            sender.send(()).unwrap()
-        }
-    }
-
-    fn spin(&mut self, receiver: std::sync::mpsc::Receiver<()>) {
-        print!("\x1B[?25l"); // hide cursor
-        let s_bar = get_unicode_string("│", "|");
-        let bar = Colors.gray(s_bar);
-        println!("{}", bar);
-        let mut frame_idx = 0;
-        let frames = &self.data;
-        let mut dot = 0;
-        loop {
-            let frame = frames[frame_idx];
-            let message = format!("{}  {} {}", Colors.magenta(frame), self.message, ".".repeat(dot.min(3)));
-            print!("\r{}\x1B[K", message);
-            frame_idx = (frame_idx + 1) % frames.len();
-            dot = (dot + 1) % (frames.len() * 4);
-            std::io::stdout().flush().unwrap();
-            thread::sleep(Duration::from_millis(100));
-            if let Ok(_) = receiver.try_recv() {}
-        }
-    }
+pub fn spinner(message: &str) -> Spinner {
+    let s_bar = get_unicode_string("│", "|");
+    println!("{}", Colors.gray(s_bar));
+    let config = SpinnerConfig::new(SpinnerStyle::CircleHalves, message)
+        .with_style_color(Color::Magenta)
+        .build();
+    Spinner::new(config)
 }
