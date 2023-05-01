dbc43c35ffb508b619483ae690aebda75050726f
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-01 08:44:08 +0300

    The changes in this commit seem to be refactoring of the `run` function in the `commit` module.
    The function now no longer uses the `async_recursion` macro and instead uses a loop to iterate through the code until a certain condition is met.
    
    The condition in this case is whether there are any staged files or changed files. If there are no files, the program prompts the user to write some code and run the program again. If there are files, the program either stages all files or prompts the user to select the files to be staged.
    
    The code also includes some minor formatting changes to the output.

--- src/commands/commit/mod.rs
+++ src/commands/commit/mod.rs
@@ -5,7 +5,6 @@ use crate::utils::{generate_message, spinner, Message, MessageRole};
 use crate::git::GitRepository;
 
 use anyhow::{anyhow, Result};
-use async_recursion::async_recursion;
 use dialoguer::{theme::ColorfulTheme, Confirm, MultiSelect};
 use structopt::StructOpt;
 
@@ -22,82 +21,92 @@ fn get_prompt(config: &AutocommitConfig, diff: &str) -> String {
 }
 
 impl CommitCommand {
-    #[async_recursion]
-    pub async fn run(&self, config: &AutocommitConfig, is_stage_all_flag: bool) -> Result<String> {
+    pub async fn run(
+        &self,
+        config: &AutocommitConfig,
+        mut is_stage_all_flag: bool,
+    ) -> Result<String> {
         GitRepository::assert_git_repo().await?;
 
-        if is_stage_all_flag {
+        loop {
+            if is_stage_all_flag {
+                let changed_files = GitRepository::get_changed_files().await?;
+
+                if !changed_files.is_empty() {
+                    GitRepository::git_add(&changed_files).await?;
+                } else {
+                    return Err(anyhow!(
+                        "No changes detected, write some code and run again"
+                    ));
+                }
+            }
+
+            let staged_files = GitRepository::get_staged_files().await?;
             let changed_files = GitRepository::get_changed_files().await?;
 
-            if !changed_files.is_empty() {
-                GitRepository::git_add(&changed_files).await?;
-            } else {
+            if staged_files.is_empty() && changed_files.is_empty() {
                 return Err(anyhow!(
                     "No changes detected, write some code and run again"
                 ));
             }
-        }
 
-        let staged_files = GitRepository::get_staged_files().await?;
-        let changed_files = GitRepository::get_changed_files().await?;
+            let mut staged_spinner = spinner();
+            staged_spinner.start("Counting staged files");
+            thread::sleep(Duration::from_secs(2));
+            if staged_files.is_empty() {
+                staged_spinner.stop("No files are staged");
 
-        if staged_files.is_empty() && changed_files.is_empty() {
-            return Err(anyhow!(
-                "No changes detected, write some code and run again"
-            ));
-        }
+                let is_stage_all_and_commit_confirmed_by_user =
+                    Confirm::with_theme(&ColorfulTheme::default())
+                        .with_prompt(format!(
+                            "{}",
+                            "Do you want to stage all files and generate commit message?"
+                        ))
+                        .interact_opt()?;
 
-        let mut staged_spinner = spinner();
-        staged_spinner.start("Counting staged files");
-        thread::sleep(Duration::from_secs(2));
-        if staged_files.is_empty() {
-            staged_spinner.stop("No files are staged");
+                if let Some(true) = is_stage_all_and_commit_confirmed_by_user {
+                    is_stage_all_flag = true;
+                    continue;
+                } else if changed_files.len() > 0 {
+                    let selected_items = MultiSelect::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Select the files you want to add to the commit:")
+                        .items(&changed_files)
+                        .interact_opt()?;
 
-            let is_stage_all_and_commit_confirmed_by_user =
-                Confirm::with_theme(&ColorfulTheme::default())
-                    .with_prompt(format!(
-                        "{}",
-                        "Do you want to stage all files and generate commit message?"
-                    ))
-                    .interact_opt()?;
+                    if let Some(items) = selected_items {
+                        let files = items
+                            .iter()
+                            .map(|&i| changed_files[i].to_string())
+                            .collect::<Vec<_>>();
 
-            if let Some(true) = is_stage_all_and_commit_confirmed_by_user {
-                self.run(config, true).await
-            } else if changed_files.len() > 0 {
-                let selected_items = MultiSelect::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Select the files you want to add to the commit:")
-                    .items(&changed_files)
-                    .interact_opt()?;
-
-                if let Some(items) = selected_items {
-                    let files = items
-                        .iter()
-                        .map(|&i| changed_files[i].to_string())
-                        .collect::<Vec<_>>();
-
-                    GitRepository::git_add(&files).await?;
-                    self.run(config, false).await
+                        GitRepository::git_add(&files).await?;
+                        is_stage_all_flag = false;
+                        continue;
+                    } else {
+                        return Err(anyhow!("No files selected for staging"));
+                    }
                 } else {
-                    Err(anyhow!("No files selected for staging"))
+                    return Err(anyhow!("No files selected for staging"));
                 }
             } else {
-                Err(anyhow!("No files selected for staging"))
+                staged_spinner.stop(format!(
+                    "{} staged files:\n{}",
+                    staged_files.len(),
+                    staged_files
+                        .iter()
+                        .map(|file| format!("  {}", file))
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                ));
+
+                let staged_diff = GitRepository::get_staged_diff(&[]).await?;
+
+                let commit_message = self
+                    .generate_commit_message_from_git_diff(config, &staged_diff)
+                    .await;
+
+                return commit_message;
             }
-        } else {
-            staged_spinner.stop(format!(
-                "{} staged files:\n{}",
-                staged_files.len(),
-                staged_files
-                    .iter()
-                    .map(|file| format!("  {}", file))
-                    .collect::<Vec<_>>()
-                    .join("\n")
-            ));
-
-            let staged_diff = GitRepository::get_staged_diff(&[]).await?;
-
-            self.generate_commit_message_from_git_diff(config, &staged_diff)
-                .await
         }
     }
 
