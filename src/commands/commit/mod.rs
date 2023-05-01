c1d838a28f395f48035c0100dd4852f2bf2fd712
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-01 11:43:38 +0300

    This code seems to be part of a Rust program that automates the process of committing changes to a Git repository. There are two files shown: `src/commands/commit/generate.rs` and `src/commands/commit/mod.rs`.
    The `generate.rs` file defines a function `generate_autocommit_message()` which generates a commit message and prompts the user to confirm whether they want to commit the changes and push them to the remote repository. If the user confirms, the changes are committed and pushed to the remote. If the user cancels, the changes are not committed and pushed. The `commit_changes()` and `push_changes()` functions are helper functions that actually perform the committing and pushing respectively.
    
    The `mod.rs` file defines the `CommitCommand` struct, which represents the command that the user can enter to initiate the commit process. It also defines the `stage` module, which contains functions for staging changes to the Git repository. There is also a `get_chat_context()` function that is used to generate the message prompt that the user will see.
    
    Overall, this code seems to be a fairly comprehensive implementation of the commit process in a Git repository, with support for user prompts and confirmation dialogs.

--- src/commands/commit/mod.rs
+++ src/commands/commit/mod.rs
@@ -1,7 +1,9 @@
 use std::time::Duration;
 
-use crate::git::GitRepository;
-use crate::utils::{generate_message, outro, spinner, Message, MessageRole};
+use crate::{
+    git::GitRepository,
+    utils::{outro, spinner},
+};
 
 use anyhow::{anyhow, Result};
 use dialoguer::{theme::ColorfulTheme, Confirm, MultiSelect};
@@ -17,16 +19,7 @@ mod stage;
 #[derive(Debug, StructOpt)]
 pub struct CommitCommand {}
 
-fn get_chat_context(config: &AutocommitConfig, diff: &str) -> String {
-    let language = format!("{:?}", config.config_data.language).to_lowercase();
-    format!("Write a git commit message in present tense for the following diff without prefacing it with anything. \
-    Do not be needlessly verbose and make sure the answer is concise and to the point. \
-    The response must be in the language {}: \n{}", language, diff)
-}
-
 impl CommitCommand {
-    
-
     async fn prompt_for_selected_files(&self, changed_files: &[String]) -> Result<Vec<String>> {
         let selected_items = MultiSelect::with_theme(&ColorfulTheme::default())
             .with_prompt(format!(
@@ -106,96 +99,8 @@ impl CommitCommand {
                 ));
 
                 let staged_diff = GitRepository::get_staged_diff(&[]).await?;
-                return self.generate_autocommit_message(config, &staged_diff).await;
+                return generate::generate_autocommit_message(config, &staged_diff).await;
             }
         }
-    }
-
-    async fn generate_autocommit_message(
-        &self,
-        config: &AutocommitConfig,
-        staged_diff: &str,
-    ) -> Result<()> {
-        let mut commit_spinner = spinner();
-        commit_spinner.start("Generating the commit message");
-
-        let prompt = Message {
-            role: MessageRole::User,
-            content: get_chat_context(config, &staged_diff),
-        };
-
-        let commit_message = generate_message(&[prompt]).await?;
-        commit_spinner.stop("📝 Commit message generated successfully");
-
-        outro(&format!(
-            "Commit message:\n\
-             ——————————————————\n\
-             {}\n\
-             ——————————————————",
-            commit_message
-        ));
-
-        let preview_confirmed_by_user = Confirm::with_theme(&ColorfulTheme::default())
-            .with_prompt(format!("Do you want to commit these changes?"))
-            .interact_opt()?;
-
-        if let Some(true) = preview_confirmed_by_user {
-            self.commit_changes(&commit_message).await?;
-            let remotes = GitRepository::get_git_remotes().await?;
-
-            let remote = if remotes.len() == 1 {
-                &remotes[0]
-            } else {
-                let remote_items = remotes.iter().map(|r| r.as_str()).collect::<Vec<_>>();
-                let selected_remote = MultiSelect::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Select the remote repository to push changes to:")
-                    .items(&remote_items)
-                    .interact_opt()?;
-                if let Some(items) = selected_remote {
-                    if items.is_empty() {
-                        outro("No remote repository selected, exiting...");
-                        return Ok(());
-                    }
-                    remotes[items[0]].as_str()
-                } else {
-                    outro("No remote repository selected, exiting...");
-                    return Ok(());
-                }
-            };
-
-            let push_confirmed_by_user = Confirm::with_theme(&ColorfulTheme::default())
-                .with_prompt(format!(
-                    "Do you want to push these changes to the remote repository {}?",
-                    remote
-                ))
-                .interact_opt()?;
-
-            if let Some(true) = push_confirmed_by_user {
-                self.push_changes(remote).await?;
-            }
-        } else {
-            outro("Commit cancelled, exiting...");
-        }
-
-        Ok(())
-    }
-
-    async fn commit_changes(&self, commit_message: &str) -> Result<()> {
-        let mut commit_spinner = spinner();
-        commit_spinner.start("Committing changes...");
-        GitRepository::git_commit(&commit_message).await?;
-        commit_spinner.stop("✔ Changes committed successfully");
-        Ok(())
-    }
-
-    async fn push_changes(&self, remote: &str) -> Result<()> {
-        let mut push_spinner = spinner();
-        push_spinner.start(format!(
-            "Pushing changes to remote repository {}...",
-            remote
-        ));
-        GitRepository::git_push(&remote).await?;
-        push_spinner.stop(format!("✔ Changes pushed to remote repository {}", remote));
-        Ok(())
     }
 }
