93e2bf885ba44c1d06e57cc4ae09ddef2153ccfd
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-01 21:14:07 +0300

    The changes to the Git autocommit tool include removing a file called `generate.rs` and moving its functions into the main `mod.rs` file. Additionally, a new `ChatContext` module was added to the imports. Finally, the `prompt.rs` file was also removed, and its functions were added to the `mod.rs` file.
    The `generate.rs` file previously contained the `generate_autocommit_message` function which generated a commit message based on a provided configuration and the diff of the staged changes. This function was moved to the `CommitCommand` module in `mod.rs` and was renamed to `generate_autocommit_message`. This function now takes `&self` as the first argument since it's now a method of the `CommitCommand` struct.
    
    The `ChatContext` module was added to the imports and is used in `mod.rs`. It was likely added to allow the `generate_autocommit_message` function to generate commit messages based on a conversation between a user and a chatbot.
    
    The `prompt.rs` file was removed, and its two functions, `prompt_for_selected_files` and `prompt_to_commit_changes`, were moved to the `mod.rs` file. Both functions were updated to use `&self` as the first argument since they are now methods of the `CommitCommand` struct.
    
    Two new functions were also added to the `CommitCommand` struct. The `prompt_for_remote` function prompts the user for a remote Git repository, while the `prompt_for_push` function prompts the user to confirm if they want to push changes to the specified remote.

--- src/commands/commit/mod.rs
+++ src/commands/commit/mod.rs
@@ -1,20 +1,19 @@
 use std::time::Duration;
 
 use crate::{
+    commands::commit::chat_context::ChatContext,
     git::GitRepository,
-    utils::{outro, spinner},
+    utils::{outro, spinner, MessageRole},
 };
 use anyhow::anyhow;
-use colored::Colorize;
-use dialoguer::{theme::ColorfulTheme, Confirm};
+use colored::{Color, Colorize};
+use dialoguer::{theme::ColorfulTheme, Confirm, MultiSelect};
 use log::info;
 use structopt::StructOpt;
 
 use super::config::AutocommitConfig;
 
 mod chat_context;
-mod generate;
-mod prompt;
 
 #[derive(Debug, StructOpt)]
 pub struct CommitCommand {}
@@ -71,7 +70,7 @@ impl CommitCommand {
                     is_stage_all_flag = true;
                     continue;
                 } else if changed_files.len() > 0 {
-                    let files = prompt::prompt_for_selected_files(&changed_files).await?;
+                    let files = Self::prompt_for_selected_files(&changed_files).await?;
                     GitRepository::git_add(&files).await?;
                     is_stage_all_flag = false;
                     continue;
@@ -95,21 +94,21 @@ impl CommitCommand {
 
                 let staged_diff = GitRepository::get_staged_diff(&[]).await?;
                 let commit_message =
-                    generate::generate_autocommit_message(config, &staged_diff).await?;
+                    Self::generate_autocommit_message(config, &staged_diff).await?;
 
                 if let Ok(Some(new_message)) =
-                    prompt::prompt_to_commit_changes(config, &staged_diff, &commit_message).await
+                    Self::prompt_to_commit_changes(config, &staged_diff, &commit_message).await
                 {
                     Self::commit_changes(&new_message).await?;
-                    if let Some(remote) = prompt::prompt_for_remote().await? {
-                        if let Ok(true) = prompt::prompt_for_push(&remote) {
+                    if let Some(remote) = Self::prompt_for_remote().await? {
+                        if let Ok(true) = Self::prompt_for_push(&remote) {
                             Self::push_changes(&new_message, &remote).await?;
                             info!("Autocommit process completed successfully");
                         }
                     }
                 }
 
-                let should_continue = prompt::prompt_to_continue().await?;
+                let should_continue = Self::prompt_to_continue().await?;
                 if !should_continue {
                     outro(&format!("{}", "Exiting...".red()));
                     return Ok(());
@@ -143,5 +142,157 @@ impl CommitCommand {
             remote.green().bold()
         ));
         Ok(())
+    }
+
+    pub async fn prompt_to_commit_changes(
+        config: &AutocommitConfig,
+        staged_diff: &str,
+        commit_message: &str,
+    ) -> anyhow::Result<Option<String>> {
+        let mut message = commit_message.to_string();
+
+        loop {
+            let is_generate_new_message_confirmed_by_user =
+                Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt(format!(
+                        "{}",
+                        "Do you want to generate a new commit message?"
+                    ))
+                    .default(false)
+                    .interact()?;
+            if is_generate_new_message_confirmed_by_user {
+                let mut new_content = String::from("Suggest a professional git commit message\n");
+                new_content.push_str(&staged_diff);
+                message = Self::generate_autocommit_message(config, &new_content).await?;
+            } else {
+                break;
+            }
+        }
+
+        let preview_confirmed_by_user = Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!("Do you want to commit these changes?"))
+            .default(true)
+            .interact_opt()?;
+
+        if let Some(true) = preview_confirmed_by_user {
+            Ok(Some(message))
+        } else {
+            outro("Commit cancelled, exiting...");
+            Ok(None)
+        }
+    }
+
+    pub async fn generate_autocommit_message(
+        config: &AutocommitConfig,
+        content: &str,
+    ) -> anyhow::Result<String> {
+        const GENERATING_MESSAGE: &str = "Generating the commit message...";
+        let mut commit_spinner = spinner();
+        commit_spinner.start(GENERATING_MESSAGE);
+
+        let mut chat_context = ChatContext::get_initial_context(config);
+        chat_context.add_message(MessageRole::User, content.to_owned());
+
+        let commit_message = chat_context.generate_message().await?;
+        commit_spinner.stop("📝 Commit message generated successfully");
+
+        let separator_length = 40;
+        let separator = "—"
+            .repeat(separator_length)
+            .color(Color::TrueColor {
+                r: 128,
+                g: 128,
+                b: 128,
+            })
+            .bold();
+
+        outro(&format!(
+            "Commit message:\n{}\n{}\n{}",
+            separator, commit_message, separator
+        ));
+        Ok(commit_message)
+    }
+
+    pub async fn prompt_to_continue() -> anyhow::Result<bool> {
+        let should_continue = Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("Do you want to continue?")
+            .default(false)
+            .interact()?;
+        Ok(should_continue)
+    }
+
+    pub async fn prompt_for_remote() -> anyhow::Result<Option<String>> {
+        let remotes = GitRepository::get_git_remotes().await?;
+        if remotes.is_empty() {
+            outro("No remote repository found, exiting...");
+            return Ok(None);
+        }
+
+        if remotes.len() == 1 {
+            return Ok(Some(remotes[0].clone()));
+        }
+
+        let remote_items = remotes.iter().map(|r| r.as_str()).collect::<Vec<_>>();
+        let selected_remote = MultiSelect::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select the remote repository to push changes to:")
+            .items(&remote_items)
+            .report(false)
+            .interact_opt()?;
+
+        if let Some(items) = selected_remote {
+            if items.is_empty() {
+                outro("No remote repository selected, exiting...");
+                return Ok(None);
+            }
+            Ok(Some(remotes[items[0]].clone()))
+        } else {
+            outro("No remote repository selected, exiting...");
+            return Ok(None);
+        }
+    }
+
+    pub async fn prompt_for_selected_files(
+        changed_files: &[String],
+    ) -> anyhow::Result<Vec<String>> {
+        let selected_items = MultiSelect::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!(
+                "Select the files you want to add to the commit ({} files changed):",
+                changed_files.len().to_string().green()
+            ))
+            .items(&changed_files)
+            .interact_opt()?;
+
+        if let Some(items) = selected_items {
+            if items.is_empty() {
+                return Err(anyhow!("Please select at least one option with space"));
+            }
+
+            let files = items
+                .iter()
+                .map(|&i| changed_files[i].to_string())
+                .collect::<Vec<_>>();
+
+            Ok(files)
+        } else {
+            return Err(anyhow!("No files selected for staging"));
+        }
+    }
+
+    pub fn prompt_for_push(remote: &str) -> anyhow::Result<bool> {
+        let push_confirmed_by_user = Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!(
+                "Do you want to push these changes to the remote repository {}?",
+                remote
+            ))
+            .default(true)
+            .interact_opt()?;
+
+        if let Some(true) = push_confirmed_by_user {
+            outro(&format!("Changes pushed to remote repository {}", remote));
+            Ok(true)
+        } else {
+            outro("Push cancelled, exiting...");
+            Ok(false)
+        }
     }
 }
