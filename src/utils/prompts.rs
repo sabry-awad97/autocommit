7407c6d9c240a5ef33f7b85fe6b8e5dde264b6c1
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-03 03:26:36 +0300

    🐛 fix(prompts.rs): Refactor intro and outro functions to allow for customizable bar and text colors The `intro` and `outro` functions were refactored to use a struct containing `bar` and `text` colors instead of using hardcoded colors. The `print_intro` and `print_outro` helper functions were created to print the bars and text with the respective colors. The `outro` function was also renamed to `print_outro` to better reflect its function. Changes made: - The `intro` function now takes in a `title` and `colors` struct that contains `bar` and `text` colors. - The `outro` function was renamed to `print_outro` to better reflect its purpose. - The `print_intro` and `print_outro` functions were created to print the bars and text with the respective colors. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/utils/prompts.rs
+++ src/utils/prompts.rs
@@ -2,34 +2,56 @@ use colored::*;
 
 use crate::utils::get_unicode_string;
 
-pub fn outro(message: &str) {
+struct BarColors {
+    bar: Color,
+    text: Color,
+}
+
+fn print_outro(message: &str, colors: &BarColors) {
     let s_bar = get_unicode_string("│", "|");
     let s_bar_end = get_unicode_string("└", "—");
     println!(
         "{}\n{} {}",
-        s_bar.color(Color::TrueColor {
-            r: 128,
-            g: 128,
-            b: 128
-        }),
-        s_bar_end.color(Color::TrueColor {
-            r: 128,
-            g: 128,
-            b: 128
-        }),
-        message
+        s_bar.color(colors.bar),
+        s_bar_end.color(colors.bar),
+        message.color(colors.text)
+    );
+}
+
+fn print_intro(title: &str, colors: &BarColors) {
+    let s_bar_start = get_unicode_string("┌", "T");
+    let title_len = title.chars().count();
+    let max_len = s_bar_start.chars().count() + title_len;
+    if max_len > 80 {
+        panic!("Title is too long to fit within the bars.");
+    }
+    println!(
+        "{}  {}",
+        s_bar_start.color(colors.bar),
+        title.color(colors.text)
     );
 }
 
 pub fn intro(title: &str) {
-    let s_bar_start = get_unicode_string("┌", "T");
-    println!(
-        "{}  {}",
-        s_bar_start.color(Color::TrueColor {
+    let colors = BarColors {
+        bar: Color::TrueColor {
             r: 128,
             g: 128,
-            b: 128
-        }),
-        title
-    );
+            b: 128,
+        },
+        text: Color::White,
+    };
+    print_intro(title, &colors);
+}
+
+pub fn outro(title: &str) {
+    let colors = BarColors {
+        bar: Color::TrueColor {
+            r: 128,
+            g: 128,
+            b: 128,
+        },
+        text: Color::White,
+    };
+    print_outro(title, &colors);
 }
