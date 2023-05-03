a41ee6632d6bf28f0671993348d4d30fbfe8aa83
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-03 08:49:52 +0300

    🐛 fix(spinner.rs): Add elapsed time to progress bar output ✨ feat(spinner.rs): Add start time tracking To display the elapsed time of a task, a `start_time` field was added to `Spinner` to keep track of the start time of the task. When the task is completed, the elapsed time is calculated and displayed in the progress bar output. Changes made: - `start_time` field added to `Spinner` struct to keep track of the start time of a task. - The `start` function now resets the start time to the current time. - The `stop` function now calculates the elapsed time and adds it to the progress bar output. - The progress bar template now includes the `{elapsed}` field to display the elapsed time. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/utils/spinner.rs
+++ src/utils/spinner.rs
@@ -1,11 +1,12 @@
 use colored::{Color, Colorize};
 use indicatif::{ProgressBar, ProgressStyle};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::utils::get_unicode_string;
 
 pub struct Spinner {
     pb: ProgressBar,
+    start_time: Instant,
 }
 
 impl Spinner {
@@ -16,17 +17,30 @@ impl Spinner {
         pb.set_style(
             ProgressStyle::default_spinner()
                 .tick_chars("◐◓◑◒")
-                .template("{spinner:.magenta} {msg}")
+                .template("{spinner:.magenta} {msg} {elapsed}")
                 .unwrap(),
         );
-        Self { pb }
+        Self {
+            pb,
+            start_time: Instant::now(),
+        }
     }
 
     pub fn start(&mut self, message: &str) {
         self.pb.set_message(message.to_string());
+        self.start_time = Instant::now();
     }
 
     pub fn stop(&mut self, message: &str) {
+        let elapsed = self.start_time.elapsed();
+        let elapsed_secs = elapsed.as_secs();
+        let elapsed_millis = elapsed.subsec_millis();
+        let elapsed_str = if elapsed_secs > 0 {
+            format!("{}s {}ms", elapsed_secs, elapsed_millis)
+        } else {
+            format!("{}ms", elapsed_millis)
+        };
+
         let s_bar = get_unicode_string("│", "|").color(Color::TrueColor {
             r: 128,
             g: 128,
@@ -40,7 +54,7 @@ impl Spinner {
                 .unwrap(),
         );
         println!("{}", s_bar);
-        self.pb.finish_with_message(message.to_string());
+        self.pb.finish_with_message(format!("{} (elapsed time: {})", message, elapsed_str));
     }
 }
 
