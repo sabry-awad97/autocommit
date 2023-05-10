faeb35f471340827823f8fba770232302a537d64
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-10 13:37:00 +0300

    🔥 Remove logger module from utils The logger module is no longer needed as it is being replaced by the Rust standard logging facilities. Therefore, the module is removed from the utils module. Changes made: - The `mod logger` line and the `pub use logger::init_logger` line were removed from the `mod.rs` file in the `utils` module. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,9 @@
 mod is_unicode_supported;
-mod logger;
 mod openai;
 mod prompts;
 mod spinner;
 
 pub use is_unicode_supported::get_unicode_string;
-pub use logger::init_logger;
 pub use openai::{generate_message, Message, MessageRole};
 pub use prompts::{intro, outro};
 pub use spinner::spinner;
